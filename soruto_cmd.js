7ae130e136f8d16e21051db8c7f41a7aceb16636
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-01-26 23:12:59 +0900

    Add files via upload

---
+++ soruto_cmd.js
@@ -0,0 +1,32 @@
+﻿/*
+*Soruto Cmd Source code(Ver.1.0)
+*This is built with Soruto EJB.
+*So, you have to write script tag for loading the Soruto EJB lib. on your page.
+*/
+window.onload = function(){
+	So_foc("ut");
+	So_divIHT("res","Welcome!!<br>");
+	
+}
+function run(){
+  var uc = So_getVal("ut").split(" ");
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET',"com/" + uc[0] + ".txt", true);
+  xhr.onreadystatechange = function(){
+    if (xhr.readyState === 4 && xhr.status === 200){
+     var csv = xhr.responseText;
+	 if(csv == "404 Error"){}else{check(csv,uc);}//404 check
+    }
+    if (xhr.readyState === 4 && xhr.status === 0){
+      var csv = xhr.responseText;
+	  if(csv == "404 Error"){}else{check(csv,uc);}//404 check
+    }
+  };
+  xhr.send(null);
+};
+function check(csv,uc){
+	var ch = csv.split("*").join(uc[1]);
+	var vals = ch.split(" ");
+	if(vals[0] == "echo"){So_divIHT(vals[1]);}
+	else{So_divIHT("res","Error:\""+uc[0]+"\" そのようなコマンドは存在しません<br>");}
+}
